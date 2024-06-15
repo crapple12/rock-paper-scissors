193c00f6bad2979874e72ec09a884f83e5aa4723
crapple12 <enoestephon@gmail.com>
2024-06-15 16:41:59 -0400

    added getHumanChoice function (prompts user to input rock,paper or scissors)
    there was annoying problem but its fixed now yay

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,11 +3,19 @@ console.log("Hello World!");
 
 const rng = ["rock", "paper", "scissors"];
 
+
 function getComputerChoice() {
     const randomChoice = rng[Math.floor(Math.random() * rng.length)];
     // generates a random choice between rock, paper, and scissors before returning it below
     return randomChoice;
 }
+
+function getHumanChoice() {
+    let que = prompt("Choose between rock, paper, or scissors!");
+    alert(`Player chose ${que}`);
+}
+    getHumanChoice();
+
 
 
 
