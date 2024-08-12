8ed2db887bf83266dccb7905c29e9ee96dcc6765
crapple12 <enoestephon@gmail.com>
2024-08-12 17:13:40 -0400

    Added playGame function
    now games are done in rounds of five with the winner announced at the end

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -25,40 +25,60 @@ function playRound(humanChoice, computerChoice) {
     
     if (humanChoice == "rock" && computerChoice == "paper") {
         let winner = "Computer";
-        return (`The winner is ${winner}`);
+        computerScore++;
+        console.log (`The winner is ${winner}`); 
     } 
     else if(humanChoice == "rock" && computerChoice == "scissors") {
         let winner = "Player";
-        return (`The winner is ${winner}`);
+        humanScore++;
+        console.log (`The winner is ${winner}`);
     }   
     else if(humanChoice == "rock" && computerChoice == "rock") {
         let winner = "no one";
-        return (`The winner is ${winner}`);
+        console.log (`The winner is ${winner}`);
     }
 
     else if (humanChoice == "paper" && computerChoice == "scissors") {
         let winner = "Computer";
-        return (`The winner is ${winner}`); 
+        computerScore++;
+        console.log (`The winner is ${winner}`); 
     }   
     else if(humanChoice == "paper" && computerChoice == "rock") {
         let winner = "Player";
-        return (`The winner is ${winner}`);
+        humanScore++;
+        console.log (`The winner is ${winner}`);
     }   
     else if(humanChoice == "paper" && computerChoice == "paper") {
         let winner = "no one";
-        return (`The winner is ${winner}`);
+        console.log (`The winner is ${winner}`);
     }
 
     else if (humanChoice == "scissors" && computerChoice == "rock") {
         let winner = "Computer";
-        return (`The winner is ${winner}`);  
+        computerScore++;
+        console.log (`The winner is ${winner}`);  
     }   
     else if(humanChoice == "scissors" && computerChoice == "paper") {
         let winner = "Player";
-        return (`The winner is ${winner}`);
+        humanScore++;
+        console.log (`The winner is ${winner}`);
     }   
     else if(humanChoice == "scissors" && computerChoice == "scissors") {
         let winner = "no one";
-        return (`The winner is ${winner}`);
+        console.log (`The winner is ${winner}`);
     }
+}   
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        playRound();
+    }
+
+    console.log (`Human scored ${humanScore} in total`);
+    console.log (`Computer scored ${computerScore} in total`);
+    if(humanScore === computerScore) console.log("Draw!");
+    else if(humanScore > computerScore) console.log("Human wins!");
+    else console.log("Computer wins!");
 }
+
+playGame();
